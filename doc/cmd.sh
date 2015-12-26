b57bd90348f05ba04454315a9e334a9ee87baa17
wenbo <yiyun6674@hotmail.com>
2015-12-26 15:27:59 +0800

    added app/assets/images/funeir.gif

--- doc/cmd.sh
+++ doc/cmd.sh
@@ -4,3 +4,15 @@
 sed -i "s/三意/天地人/g" `grep '三意' -rl ./app/views`
 
 sed -i '' -e  "s/天地人品牌设计/出未创意设计/g" `grep '天地人品牌设计' -rl ./app/views`
+
+sed -i '' -e  "s/天地人/出未/g" `grep '天地人' -rl ./app/views`
+
+sed -i '' -e  "s/服务模式/服务内容/g" `grep '服务模式' -rl ./app/views`
+
+sed -i '' -e  "s/137 6189 2871/156 0187 8272/g" `grep '137 6189 2871' -rl ./app/views`
+
+ sed -i '' -e  "s/137-6189-2871/156-0187-8272/g" `grep '137-6189-2871' -rl ./app/views`
+
+
+
+
