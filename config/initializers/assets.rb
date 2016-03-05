c51eabbf134c7c6731ce9c55544143c952a84435
wenbo <yiyun6674@hotmail.com>
2016-03-05 18:03:41 +0800

    Rails.application.config.assets.precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,3 @@
 Rails.application.config.assets.precompile += %w( admin_init.css admin.css  Contact_us.css  cooperate.css  index.css  list.css  page.css   Team.css
-admin_init.css  base.css content.css global.css libs mode.css  reset.css)
-Rails.application.config.assets.precompile += %w( admin_init.js )
+admin_init.css  base.css content.css global.css libs mode.css  reset.css skin.css)
+Rails.application.config.assets.precompile += %w( admin_init.js jquery-1.4.2.min.js jquery.jcarousel.min.js main.js)
